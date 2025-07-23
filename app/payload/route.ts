fbf78e2f446bdb893ec6cd83b598dc9611f486e9
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-23 23:18:37 +0200

    Create route.ts

---
+++ app/payload/route.ts
@@ -0,0 +1,7 @@
+import { serve } from '@payloadcms/next-payload';
+
+export const GET = serve;
+export const POST = serve;
+export const PUT = serve;
+export const PATCH = serve;
+export const DELETE = serve;
