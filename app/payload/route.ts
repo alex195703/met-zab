b0dacf3159198425b5ebb129a305a4f9a399d3a7
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-24 00:57:12 +0200

    Update route.ts

--- app/payload/route.ts
+++ app/payload/route.ts
@@ -1,6 +1,7 @@
 import { getHandler } from 'payload/config';
 
 export const GET = getHandler;
+export const POST = getHandler;
 export const PUT = getHandler;
 export const PATCH = getHandler;
 export const DELETE = getHandler;
