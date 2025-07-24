56f2d2505561efe702d3be2cede95c67543530f9
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-24 23:22:47 +0200

    Update route.ts

--- app/payload/route.ts
+++ app/payload/route.ts
@@ -1,4 +1,11 @@
-import { getPayload } from '@payloadcms/next';
+// app/payload/[[...slug]]/route.ts
+import { getPayloadHandler } from '@payloadcms/next'
+import config from '../../../payload.config'
 
-export const GET = getPayload;
-export const POST = getPayload;
+const handler = getPayloadHandler({ config })
+
+export const GET = handler
+export const POST = handler
+export const PUT = handler
+export const DELETE = handler
+export const PATCH = handler
