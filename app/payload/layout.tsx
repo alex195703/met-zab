7b4f438f44c1740d748ef8b2822884ee02d676b3
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-25 15:54:31 +0200

    Update layout.tsx

--- app/payload/layout.tsx
+++ app/payload/layout.tsx
@@ -1,5 +1,34 @@
-import  AdminView  from 'payload'
+import { notFound } from 'next/navigation';
+import config from '../../../src/payload.config';
+import { getPayloadHMR } from '@payloadcms/next/utilities';
+import { Payload } from 'payload';
 
-export default function Admin() {
-  return <AdminView />
+export default async function AdminLayout() {
+  let payload: Payload;
+
+  try {
+    payload = await getPayloadHMR({
+      config,
+    });
+  } catch (error) {
+    return notFound();
+  }
+
+  // Отримуємо URL адмін-панелі
+  const adminURL = `${process.env.NEXT_PUBLIC_SERVER_URL}/admin`;
+
+  return (
+    <html lang="en">
+      <body>
+        <iframe
+          src={adminURL}
+          style={{
+            border: 'none',
+            width: '100%',
+            height: '100vh',
+          }}
+        />
+      </body>
+    </html>
+  );
 }
