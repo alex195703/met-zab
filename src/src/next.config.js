4bfa3729ffeac151600b09b9d99e15c29e01b1f9
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-28 01:20:45 +0200

    Delete src/src directory

--- src/src/next.config.js
+++
@@ -1,8 +0,0 @@
-import { withPayload } from '@payloadcms/next';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withPayload(nextConfig, {
-  configPath: './payload/config.ts', // Вказуємо шлях до конфігурації Payload
-});
