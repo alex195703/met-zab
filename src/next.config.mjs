1db61cf1e2954fc2f295ac4f1890d5a591953958
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-28 21:00:23 +0200

    Update next.config.mjs

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,5 +1,4 @@
-import { withPayload } from '@payloadcms/next/config';
-
+import { withPayload } from '@payloadcms/next/withPayload';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
