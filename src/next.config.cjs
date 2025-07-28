6080018e325214ace2cfc633f1e5de6306c33112
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-28 19:42:02 +0200

    Update and rename next.config.cjs to next.config.mjs

--- src/next.config.cjs
+++
@@ -1,7 +0,0 @@
-const { withPayload } = require('@payloadcms/next');
-
-const nextConfig = {};
-
-module.exports = withPayload(nextConfig, {
-  configPath: './payload/config.ts', // Вказуємо шлях до конфігурації Payload
-});
