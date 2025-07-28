2d9f25e5b4e5479cbf3faf8fdaee85b4a70fe599
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-07-28 18:19:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
+import { withPayload } from '@payloadcms/next';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
-      })
-    );
-    return config;
-  },
-  serverExternalPackages: ['payload', '@payloadcms/next', '@payloadcms/db-postgres'],
-};
+const nextConfig = {};
 
-import { withPayload } from '@payloadcms/next/withPayload';
-
-export default withPayload(nextConfig);
+export default withPayload(nextConfig, {
+  configPath: './payload/config.ts', // Вказуємо шлях до конфігурації Payload
+});
