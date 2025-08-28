b857fb4da4c800b960a50258e0c528d991481319
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-08-29 01:07:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-import { withPayload } from '@payloadcms/next';
+import { withPayload } from '@payloadcms/next/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default withPayload(nextConfig, {
-  configPath: './payload/config.ts', // Вказуємо шлях до конфігурації Payload
+  configPath: './payload/config.ts', // шлях до конфігурації Payload
 });
