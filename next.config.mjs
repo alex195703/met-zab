ba43dd294117d8b561b58adacec98f56c5c124d7
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-08-29 10:59:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,16 @@ import { withPayload } from '@payloadcms/next/config'
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true,
-  },
+    serverActions: true
+  }
 }
 
 export default withPayload(nextConfig, {
+  // Шлях до payload.config.ts у корені проекту
   configPath: './payload.config.ts',
+
+  // Вказуємо URL сервера (важливо для адмінки на Vercel)
+  payloadConfig: {
+    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  }
 })
