dea46e9d665b92833b3d5c9bf76bb93c5a83b09e
alex195703 <62181211+alex195703@users.noreply.github.com>
2025-08-29 20:07:18 +0200

    Update course-footer.tsx

--- components/course-footer.tsx
+++ components/course-footer.tsx
@@ -24,7 +24,7 @@ const quickLinks = [
   { name: "Репозитарій університету", url: "#" }
 ];
 
-export function CourseFooter() {
+export default function CourseFooter() {
   console.log("CourseFooter component rendered");
 
   return (
